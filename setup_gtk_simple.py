8939a6a0acba83f7378f38d33a20d7679be56462
olivier DREVET <odrevet@gmail.com>
2015-08-12 20:15:09 +0200

    indent

--- setup_gtk_simple.py
+++ setup_gtk_simple.py
@@ -15,23 +15,23 @@ packages = ['gi']
 
 #list of dll to copy
 dll_list = ['libatk-1.0-0.dll',
-			'libffi-6.dll',
-			'libfontconfig-1.dll',
-			'libfreetype-6.dll',
-			'libgio-2.0-0.dll',
-			'libgirepository-1.0-1.dll',
-			'libglib-2.0-0.dll',
-			'libgmodule-2.0-0.dll',
-			'libintl-8.dll',
-			'libgobject-2.0-0.dll',
-			'libgtk-3-0.dll',
-			'libharfbuzz-gobject-0.dll',
-			'libjasper-1.dll',
-			'libpng16-16.dll',
-			'librsvg-2-2.dll',
-			'libwebp-5.dll',
-			'libwinpthread-1.dll',
-			'libxmlxpat.dll',
+            'libffi-6.dll',
+            'libfontconfig-1.dll',
+            'libfreetype-6.dll',
+            'libgio-2.0-0.dll',
+            'libgirepository-1.0-1.dll',
+            'libglib-2.0-0.dll',
+            'libgmodule-2.0-0.dll',
+            'libintl-8.dll',
+            'libgobject-2.0-0.dll',
+            'libgtk-3-0.dll',
+            'libharfbuzz-gobject-0.dll',
+            'libjasper-1.dll',
+            'libpng16-16.dll',
+            'librsvg-2-2.dll',
+            'libwebp-5.dll',
+            'libwinpthread-1.dll',
+            'libxmlxpat.dll',
             'libcairo-gobject-2.dll',
             'libgdk-3-0.dll',
             'libgdk_pixbuf-2.0-0.dll',
@@ -41,7 +41,7 @@ dll_list = ['libatk-1.0-0.dll',
             'libpangocairo-1.0-0.dll',
             'libpangoft2-1.0-0.dll',
             'libpangowin32-1.0-0.dll',
-			'libzzz.dll']
+            'libzzz.dll']
 for dll in dll_list:
     include_files.append((os.path.join(gnome_dir, dll), dll))  
 
@@ -51,10 +51,10 @@ include_files.append((os.path.join(gnome_dir, 'lib/girepository-1.0'), 'lib/gire
 
 #copy subdirectories
 gnome_subdirs = [
-			'etc', 
-			#'lib', 
-			#'share'
-			]
+    'etc', 
+    #'lib', 
+    #'share'
+]
 for gnome_subdir in gnome_subdirs:
     include_files.append((os.path.join(gnome_dir, gnome_subdir), gnome_subdir))
 
@@ -63,10 +63,10 @@ executables = [Executable('gtk_simple.py', base=base)]
 setup(name='gtk_simple',
       version = '1.0',
       description = '',
-	  options = {"build_exe": {'includes': includes,
-		    					'excludes': excludes,
-								'packages': packages,
-								'include_files':include_files,
-								 }
+      options = {"build_exe": {'includes': includes,
+                               'excludes': excludes,
+                               'packages': packages,
+                               'include_files':include_files,
+                           }
 				},
       executables = executables)
